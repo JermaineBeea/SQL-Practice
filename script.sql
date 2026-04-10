d43da77f54eb9f627ec013127e162f9a2d0aa18d
Jermaine Beea <jermainebeea@gmail.com>
2026-04-10 15:39:06 +0200

    modifications made

--- script.sql
+++ script.sql
@@ -1,12 +1,11 @@
-
-WITH reference AS (
-    SELECT *, (
-        inventory.price * purchases.quantity_purchased
-    ) AS total_expenditure
-    FROM purchases LEFT JOIN inventory ON purchases.product_name = inventory.product_name
-)
-
-SELECT customer_email, SUM(reference.total_expenditure) AS gross_expenditure
-FROM reference
-GROUP BY reference.customer_email;
-
+SELECT
+    id,
+    customer_email,
+    date_purchase,
+    quantity_purchased,
+    quantity_purchased
+      - LAG(quantity_purchased) OVER (
+            PARTITION BY customer_email
+            ORDER BY date_purchase, id
+        ) AS change_from_last_purchase
+FROM purchases;
